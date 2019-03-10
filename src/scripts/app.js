5a927e02be07fe5db3cb40c00ef9f6e407c4c0fe
Marcin <marcin.aleksanderek@gmail.com>
2019-03-10 20:27:11 +0100

    WDP190203-19

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,22 @@
 console.log('hello');
+
+Array.from(document.querySelectorAll('.stars')).forEach(function (a) {
+  a.addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log(e);
+    e.originalTarget.classList.add('full');
+    var previousSibling = e.originalTarget.nextElementSibling;
+    var nextSibling = e.originalTarget.previousElementSibling;
+    // Direction jest ustawione na rtl wiec next jest tak naprawde previous ;) (i odwrotnie)
+    while (previousSibling) {
+      previousSibling.classList.add('full');
+      previousSibling.classList.remove('voted');
+      previousSibling = previousSibling.nextElementSibling;
+    }
+    // Petle wywolujemy dwa razy zeby usunac klase w przypadku nowego glosowania
+    while (nextSibling) {
+      nextSibling.classList.remove('full');
+      nextSibling = nextSibling.previousElementSibling;
+    }
+  });
+});
