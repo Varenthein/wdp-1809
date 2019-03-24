ec2ca90acbd45f36f986d9cc8fbd11bb57649f32
Marcin <marcin.aleksanderek@gmail.com>
2019-03-24 15:03:17 +0100

    WDP190203-19

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,11 @@
 console.log('hello');
 
-Array.from(document.querySelectorAll('.stars')).forEach(function (a) {
-  a.addEventListener('click', function (e) {
-    e.preventDefault();
-    console.log(e);
-    e.originalTarget.classList.add('full');
-    var previousSibling = e.originalTarget.nextElementSibling;
-    var nextSibling = e.originalTarget.previousElementSibling;
+Array.from(document.querySelectorAll('.stars')).forEach(function (starLink) {
+  starLink.addEventListener('click', function (clickEvent) {
+    clickEvent.preventDefault();
+    clickEvent.target.classList.add('full');
+    var previousSibling = clickEvent.target.nextElementSibling;
+    var nextSibling = clickEvent.target.previousElementSibling;
     // Direction jest ustawione na rtl wiec next jest tak naprawde previous ;) (i odwrotnie)
     while (previousSibling) {
       previousSibling.classList.add('full');
